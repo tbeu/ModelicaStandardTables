9c4d3d8172a4781c5826205ca41844221e50044d
tbeu <tc@tbeu.de>
2015-12-28 21:44:32 +0100

    Fix typos

--- C-Sources/ModelicaMatIO.h
+++ C-Sources/ModelicaMatIO.h
@@ -135,7 +135,7 @@ typedef uint8_t mat_uint8_t;
 #endif
 
 /** @defgroup MAT Matlab MAT File I/O Library */
-/** @defgroup mat_util MAT File I/O Utitlity Functions */
+/** @defgroup mat_util MAT File I/O Utility Functions */
 /** @if mat_devman @defgroup mat_internal Internal Functions @endif */
 
 /** @brief MAT file access types
@@ -300,7 +300,7 @@ typedef struct mat_sparse_t {
                                *  data[k].  0 <= k <= nzmax
                                */
     int nir;                 /**< number of elements in ir */
-    int *jc;                 /**< Array size N+1 (N is number of columsn) with
+    int *jc;                 /**< Array size N+1 (N is number of columns) with
                                *  jc[k] being the index into ir/data of the
                                *  first non-zero element for row k.
                                */
